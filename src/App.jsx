29d7fdcf64ee706990df90f1e4b7c352a800740d
anas20023 <yorder07@gmail.com>
2023-12-28 04:14:07 +0600

    Add heading component and fix typo in items.jsx and item_2.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Top_bar from './top';
 import Item_Add from './items';
 import Item_Add_2 from './item_2';
+import Heading from './head_Section';
 export default function Div() {
   return (
     <div className="flex flex-col justify-between items-center px-4">
+      <Heading></Heading>
       <Top_bar />
       <Item_Add/>
       <Item_Add_2/>
---
+++ src/head_Section.jsx
@@ -0,0 +1,7 @@
+export default function Heading() {
+  return (
+    <h1 className="text-4xl font-bold text-center m-5 text-gray-800">
+      Todo App
+    </h1>
+  );
+}
