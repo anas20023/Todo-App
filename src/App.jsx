5cf49ea36c4581adb2f126d783a11920c9ab1d5e
anas20023 <yorder07@gmail.com>
2024-06-20 15:12:37 +0600

    chore: Update item components and import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import Top_bar from './components/top';
-import Item_Add from './components/items';
+import Items from './components/items';
 import Heading from './components/head_Section';
 export default function Div() {
+  const itemarr = [{
+    name: "Going To School",
+    Date: "10-20-2003"
+  },
+  {
+    name: "Going To College",
+    Date: "10-20-2003"
+  },
+  {
+    name: "Going To University",
+    Date: "10-20-2003"
+  },
+  ]
   return (
     <div className="flex flex-col justify-between items-center px-4">
       <Heading></Heading>
       <Top_bar />
-      <Item_Add ev_name="School Going" ev_date="01-02-2002" />
-      <Item_Add ev_name="Not School Going" ev_date="01-02-2004" />
+      <Items items={itemarr} />
     </div>
   );
 }
--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,17 +1,18 @@
-export default function Item_Add(obj) {
-  let event_name = obj.ev_name;
-  let event_date = obj.ev_date;
-  return (
-    <div className="flex flex-row w-full sm:w-full lg:w-2/4 mt-5 h-10 justify-between items-center gap-1.5">
-      <p className="w-2/5 px-3 py-4 text-lg text-gray-700 font-semibold">
-        {event_name}
-      </p>
-      <p className="w-2/5 px-3 py-4 text-baseg font-bold text-gray-800">
-        {event_date}
-      </p>
-      <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-1/5 px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-        Delete
-      </button>
-    </div>
-  );
-}
+/* eslint-disable react/prop-types */
+import Item from './Item';
+
+const Items = ({ items }) => (
+  <>
+    {items.map((item, index) => {
+      return (
+        <Item
+          key={index}
+          eventName={item.name}
+          eventDate={item.Date}
+        />
+      );
+    })}
+  </>
+);
+
+export default Items;
